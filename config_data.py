7acb92159cb123b3019a56d89dbfd4b55a55c9fc
marykatenussrallah <mnussrallah2@huskers.unl.edu>
2024-12-05 22:46:56 -0600

    debugging

--- config_data.py
+++ config_data.py
@@ -9,7 +9,8 @@ player_data = [
     ["Random", "2.png", colors.ORANGE],
    ["DFS", "3.png", colors.PURPLE],
    ["BFS", "4.png", colors.BLUE],
-    ["Dijkstra", "5.png", colors.GREEN]
+    ["Dijkstra", "5.png", colors.GREEN],
+    ["Floyd-Warshall", "6.png", colors.GRAY]
 ]
 
 display_size_right = 400
